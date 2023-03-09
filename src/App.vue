9c71e20e01438adfc9b77c9c57e384846115d50f
Eranmonnie <ajalaferanmi123@gmail.com>
2023-03-09 18:49:52 +0100

    implement counter feature

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script>
 export default{
 
-data(){
+data:()=>{
 
 return{
 
   name : "ajala feranmi",
+  val:0,
   lists :[
     { 
       namee:"tom",
@@ -28,7 +29,16 @@ favouritefood:"Rice"
 
 }],
 
-}}
+}},
+
+methods:{
+  
+  increment(){
+    this.val++
+  }
+
+
+}
 
 }
 
@@ -37,12 +47,17 @@ favouritefood:"Rice"
 
 <template>
 
-  <h1>{{ name }}</h1>
+  <h1 v-if="name.length %2 === 0">{{ name }}</h1>
+  <h1 v-else>name if odd</h1>
+
   <ul  v-for="list in lists">
     <li>{{ list.namee }}</li>
     <li>{{ list.skintone }}</li>
     <li>{{ list.favouritefood }}</li>
   </ul>
 
+<h1>Counter</h1>
+<h1>{{ val }}</h1>
+<button @click="increment">Increment</button>
 </template>
 
