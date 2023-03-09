5ec14673b569b40b23a1a082ad1743a06c5f7dd6
Eranmonnie <ajalaferanmi123@gmail.com>
2023-03-09 19:51:22 +0100

    implement favourite list feature

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,16 @@ export default{
 data:()=>{
 
 return{
+  favouritecharacter:[
 
+  {name:"Zuko"},
+  {name:"kira"},
+  {name:"Naruto"}
+
+  ],
+
+  newcharacter:{name:""},
+  favcharlist:[],
   name : "ajala feranmi",
   increamm:1,
   val:0,
@@ -36,7 +45,16 @@ methods:{
 
   increment(){
     this.val += this.increamm
-  } 
+  },
+
+appendlist(value){
+  this.favcharlist.push(value)
+},
+
+addtocharacterlist(){
+  this.favouritecharacter.push(this.newcharacter)
+  this.newcharacter={name:""}
+}
 
 
 }
@@ -63,6 +81,22 @@ methods:{
 <label for="incremantammount"> Increment by:</label>
 <input type="number" v-model="increamm">
 
+<h1>append list</h1>
+
+<ul>
+
+<li v-for="fav in favouritecharacter">
+
+<p>{{ fav.name }}</P>
+<button @click="appendlist(fav.name)">Add</button>
+
+</li>
+</ul>
+<input type="text" v-model="newcharacter.name" @keyup.enter="addtocharacterlist">
+
+<h1>favourite characters</h1>
+<h1 v-if="favcharlist.length === 0">No favourite list</h1>
+<h1 v-else v-for="characters in favcharlist">{{ characters }}</h1>
 
 </template>
 
