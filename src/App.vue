6bf635b730b4b048c4842c10a10ef56514c6e46b
Eranmonnie <ajalaferanmi123@gmail.com>
2023-03-13 19:33:45 +0100

    make navbar for generic component exercise

--- src/App.vue
+++ src/App.vue
@@ -1,101 +1,47 @@
 <script>
-import Counter from "./components/counter.vue"
-import list from "./components/list.vue"
-import name from "./components/name.vue"
-import slotproject from "./components/slotproject.vue"
-import async from "./components/async.vue"
+import homepage from "./components/homepage.vue"
 export default{
+
   components:{
-    Counter,
-    list,
-    name,
-    slotproject,
-    async,
+    homepage,
   },
 
-data:()=>{
-
-return{
-  favouritecharacter:[
-  {name:"Naruto"},
-  {name:"Zuko"},
-  {name:"kira"},
-
-  ],
-  newcharacter:{name:""},
-  favcharlist:[],
-  name : "ajala feranmi",
-  lists :[
-    { 
-      namee:"tom",
-  skintone:"Black",
-favouritefood:"Rice"
-
-},
-
-{ 
-      namee:"tom",
-  skintone:"Black",
-favouritefood:"Rice"
-
-},
-
-{ 
-      namee:"tom",
-  skintone:"Black",
-favouritefood:"Rice"
-
-}],
-
-}},
-
-methods:{
-changenameto(){
-  this.name =  "new name"
-},
-
-
-},
-
-computed:{
-
- 
-}
 
 }
-
 
 </script>
 
 <template>
-
-  <h1 v-if="name.length %2 === 0">{{ name }}</h1>
-  <h1 v-else>name if odd</h1>
-
-  <ul  v-for="list in lists">
-    <li>{{ list.namee }}</li>
-    <li>{{ list.skintone }}</li>
-    <li>{{ list.favouritefood }}</li>
-  </ul>
-
-<counter/>
-
-<list v-bind:favouritecharacter="favouritecharacter" v-bind:newcharacter="newcharacter" v-bind:favcharlist="favcharlist" />
-
-<name  :name="name" @changename="changenameto"/>
-
-<slotproject>
-  <template v-slot:case>
-  hiiiii
-  </template>
-
-  <template v-slot:case2>
-  hiiiii
-  </template>
-</slotproject>
-
-<async />
+  <nav class="navbar">
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+    </ul>
+  </nav>
+<homepage />
+  
 </template>
 
 
-
+<style>
+.navbar{
+  border: 1px  solid transparent;
+  border-radius: 1em;
+  width: 75%;
+  margin: 0 auto;
+  padding-left: 1em;
+  padding-right: 2em;
+  background-color: rgb(238, 236, 212);
+}
+ul{
+  list-style: none;
+  display: flex;
+  align-items: center;
+  justify-content: right;
+  gap: 2em;
+}
+a{
+  text-decoration: none;
+  color: black;
+}
+</style>
