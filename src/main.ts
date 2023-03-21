3fc1f38730c322e3ff8ee6e3accb948586124a4e
Eranmonnie <ajalaferanmi123@gmail.com>
2023-03-21 12:33:43 +0100

    state management with pinia

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import { createApp } from 'vue'
 import {createRouter, createWebHashHistory} from 'vue-router'
 import {routes} from "./router"
+import {createPinia} from "pinia"
 import App from './App.vue'
 
 
+
+
+const pinia =createPinia()
 const router = createRouter({
     history:createWebHashHistory(),
     routes, 
 })
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(pinia).mount('#app')
