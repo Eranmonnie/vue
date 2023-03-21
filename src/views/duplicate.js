6742b9efad9261ecdaadd2bec7b1bf5870aa4665
Eranmonnie <ajalaferanmi123@gmail.com>
2023-03-21 12:35:18 +0100

    state management with pinia

---
+++ src/views/duplicate.js
@@ -0,0 +1,16 @@
+import {defineStore} from "pinia"
+export const countstore =defineStore("duplicate",{
+// like data in options
+    state:()=>{
+
+    },
+// like computed in options 
+    getters:{
+
+    },
+// like methods in options
+    actions:{
+
+    }
+})
+// we impoturt it into the document we need instantiate it and thata it 
